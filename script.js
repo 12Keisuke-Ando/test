d44c908dfae7dcd5cb186a4c1d16e601de83608a
12Keisuke-Ando <k.kshoa.mi.sho@icloud.com>
2022-05-13 21:08:00 +0900

    20220513

---
+++ script.js
@@ -0,0 +1,85 @@
+document.write("ココカラ―――――――");
+document.write("<br>");
+for(var i =1; i <= 5; i++){
+    document.write("★");
+}
+document.write("<br>");
+document.write("―――――――ココマデ");
+
+document.write("<br>");
+document.write("ココカラ―――――――");document.write("<br>");
+for(var star1 =1; star1 <= 2; star1 ++){
+    for(var star2 = 0; star2 < 3; star2 ++){
+        document.write("★");
+    }
+    document.write("<br>");
+}document.write("―――――――ココマデ");
+
+document.write("<br>");
+document.write("ココカラ―――――――");document.write("<br>");
+for(var star3 = 1; star3 < 3; star3 ++){
+    for(var star4 = 2; star4 <= 6; star4 ++){
+        document.write("☆");
+    }
+    document.write("<br>");
+}document.write("―――――――ココマデ");
+
+document.write("<br>");
+document.write("ココカラ―――――――");document.write("<br>");
+for(var star5 = 1; star5 <=4; star5 ++){
+    for(var star6 = 1; star6 <= 5; star6 ++){
+        document.write("★");
+    }
+    document.write("<br>");
+}document.write("―――――――ココマデ");
+
+document.write('<br>');
+document.write("ココカラ―――――――");document.write("<br>");
+for(var star7 = 1; star7 <=4; star7 ++){
+    for(var star8 = 1; star8 <= 3; star8 ++){
+        document.write("★");
+    }
+    document.write("<br>");
+}document.write("―――――――ココマデ");
+
+document.write('<br>');
+document.write("ココカラ―――――――");
+document.write("<br>");
+for(var star9 = 0; star9 <= 2; star9 ++){
+    for(var star10 = 1; star10 <= 3; star10 ++){
+        if(star10 % 2 == 1){
+            document.write("★");
+        }else{
+            document.write("☆");
+        }
+    }
+    document.write("<br>");
+}
+document.write("―――――――ココマデ");
+    
+document.write('<br>');
+document.write("ココカラ―――――――");
+document.write("<br>");
+for(var star11 =6; star11 < 10; star11 ++){
+    for(var star12= 6; star12 <= 10; star12 ++){
+        if(star12 % 2 == 0){
+            document.write("★");
+        }else{
+            document.write("☆");
+        }
+    }
+    document.write("<br>");
+}
+document.write("―――――――ココマデ");
+    
+document.write('<br>');
+document.write("ココカラ―――――――");
+document.write("<br>");
+for(var x = 0; x < 5; x ++){
+    for(var y = 0; y < x+1; y ++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+document.write("―――――――ココマデ");
+
